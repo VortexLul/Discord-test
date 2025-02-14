db165650af2d94cee58f85e987e3712a77eaf14c
VortexLule <mrtimofe.yuhimenko@gmail.com>
2025-02-14 13:10:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+
+namespace Discord_test
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+        public static async Task Main()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = "MTIwODA3OTQxNDU4ODM0MjMzMw.Gmq2DQ.Wg0wi3mHkx0kOIT4DUy5MApYQ3GR2mViZ_CV_Y";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public class LoggingService
+        {
+            public LoggingService(DiscordSocketClient client, CommandService command)
+            {
+                client.Log += LogAsync;
+                command.Log += LogAsync;
+            }
+            private Task LogAsync(LogMessage message)
+            {
+                if (message.Exception is CommandException cmdException)
+                {
+                    Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+                        + $" failed to execute in {cmdException.Context.Channel}.");
+                    Console.WriteLine(cmdException);
+                }
+                else
+                    Console.WriteLine($"[General/{message.Severity}] {message}");
+
+                return Task.CompletedTask;
+            }
+        }
+            private static async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+            {
+                var message = await before.GetOrDownloadAsync();
+                Console.WriteLine($"{message} -> {after}");
+            }
+        }
+
+    }
